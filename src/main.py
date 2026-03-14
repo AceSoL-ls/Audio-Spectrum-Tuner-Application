4c493e5c1ea8a7b689f6fc18e0294ecd800042e3
AceSoL-ls <eyris97@gmail.com>
2026-03-14 20:24:17 +0200

    Imported all the needed libraries for our project to work

---
+++ src/main.py
@@ -0,0 +1,17 @@
+import sys
+import PyAudio
+import NumPy
+import SciPy
+import PyInstaller
+
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
